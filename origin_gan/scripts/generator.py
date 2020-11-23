de091073b9453d080cbcb0293a3d71574c6a6a2e
Guoliang HE <guolianghe1996@gmail.com>
2020-11-23 15:11:05 +0000

    generator

--- origin_gan/scripts/generator.py
+++ origin_gan/scripts/generator.py
@@ -1,1 +1,47 @@
 import tensorflow as tf
+import tensorflow.keras as kr
+
+
+class generator:
+
+    """
+    encapsulated class for generator 
+    """
+
+    def __init__(self, 
+                model,  # a generator model for training
+                ):
+        super().__init__()
+
+        self.model = model
+
+    
+    def training(self):
+        return 
+
+    def test(self):
+        return 
+
+
+
+class _generator_model(kr.Model):
+
+    def __init__(self):
+        super().__init__()
+
+    
+    def call(self, x):
+        return x;
+
+    
+
+
+
+
+if __name__ == "__main__":
+
+    """
+    generate toy data to test the generator
+    """
+
+    
