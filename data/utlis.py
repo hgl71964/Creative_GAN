5f261a9539e8d30eda813b7789b3354add4059a7
Guoliang HE <guolianghe1996@gmail.com>
2020-11-26 17:24:31 +0000

    fix

--- data/utlis.py
+++ data/utlis.py
@@ -27,30 +27,3 @@ class img_reader:
     def N(self):
         return len(self.img_list)
     
-
-
-"""  use matplotlib to process """
-# simg = mpimg.imread(f"{os.path.join(data_path, img_list[0])}")
-# print(simg.shape)  #  numpy data
-
-# #  plot the data
-# imgplot = plt.imshow(simg)
-# plt.show()
-
-"""use PIL package to resize"""
-image = PIL.Image.open(f"{os.path.join(data_path, img_list[0])}")
-# image.show()  #  plot image
-
-print(image.format) 
-print(image.mode) 
-print(image.size) 
-print(image.palette) 
-new_image = image.resize((400, 400))
-new_image.show()
-
-image.thumbnail((400,400))  #  preserve aspect ratio
-print(image.format) 
-print(image.mode) 
-print(image.size) 
-print(image.palette) 
-image.show()
