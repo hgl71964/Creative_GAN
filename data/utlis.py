6635a9b10ffcd7c4285f8f0030cdae73fd4e382c
Guoliang HE <guolianghe1996@gmail.com>
2020-11-27 00:17:38 +0000

    fix

--- data/utlis.py
+++ data/utlis.py
@@ -15,7 +15,7 @@ class img_reader:
         for i in os.listdir(f"{self.path}"):
             if i[-4:]=="jpeg":
                 self.img_list.append(i)
-                
+
     def get_jpg(self):
         self.img_list = []
         for i in os.listdir(f"{self.path}"):
@@ -44,18 +44,20 @@ class img_reader:
         for i in range(self.N):
             self.images[i] = self.images[i].resize(*size)
 
-    def to_tensor(self):
+    def to_tensor(self, normalisation = True):
         if not self.images: 
             print("Haven't loaded images")
         else:
             for i in range(self.N):
                 t = tf.convert_to_tensor(np.array(self.images[i]))
-
-                s = tf.reduce_sum(t,axis=2)  #  normalisation
-                r = t[:,:,0]/s
-                g = t[:,:,1]/s, 
-                b = t[:,:,2]/s
-                self.images[i]=tf.concat((tf.reshape(r,(224,224,1)),tf.reshape(g,(224,224,1)), tf.reshape(b,(224,224,1))),axis=2)
+                if normalisation:
+                    s = tf.reduce_sum(t,axis=2)  #  normalisation
+                    r = t[:,:,0]/s
+                    g = t[:,:,1]/s, 
+                    b = t[:,:,2]/s
+                    self.images[i]=tf.concat((tf.reshape(r,(224,224,1)),tf.reshape(g,(224,224,1)), tf.reshape(b,(224,224,1))),axis=2)
+                else:
+                    self.images[i] = t
 
 
     def batcher(self, batch_size):
